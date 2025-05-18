22ceb14348f15e11d1e36bb7951dc2e18d61e4f8
かりこ <webcreate0303yokoi0630@gmail.com>
2025-05-18 23:21:32 +0900

    ui :heavy_plus_sign: PickupProject作成／見た目実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+// ---- CSS ----
+import './globals.css';
+// ---- Components ----
+import PickupProject from '@/features/PickupProject';
+
 export default function Home() {
   return (
-  <h1>Hello World!</h1>
+    <>
+      <h1>稼働時間管理アプリ</h1>
+      <PickupProject />
+    </>
   );
 }
