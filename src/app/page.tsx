95a10d22681645e4245f97815d7fea77a2334c60
かりこ <webcreate0303yokoi0630@gmail.com>
2025-06-02 21:22:47 +0900

    chore :building_construction: pageへLogsTableを配置／レイアウト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,18 @@
 import './globals.css';
 // ---- Components ----
 import PickupProject from '@/features/PickupProject';
+import LogsTable from '@/features/logs/LogsTable';
 
 export default function Home() {
   return (
-    <>
-      <h1>稼働時間管理アプリ</h1>
-      <PickupProject />
-    </>
+    <div className="py-[10px] px-[2dvw]">
+      <header className="mb-[14px]">
+        <h1 className="text-[12px]">稼働時間管理アプリ</h1>
+        <PickupProject />
+      </header>
+      <main>
+        <LogsTable />
+      </main>
+    </div>
   );
 }
